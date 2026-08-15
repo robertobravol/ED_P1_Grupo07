9c36f9c3d1bc5e42a7a1c6668c62782e169d70f3
Eddy Arriaga <eddyearriaga@gmail.com>
2026-08-15 15:56:36 -0500

    activity main realizado

--- app/src/main/java/com/espol/ed_p1_grupo07/MainActivity.java
+++ app/src/main/java/com/espol/ed_p1_grupo07/MainActivity.java
@@ -1,12 +1,15 @@
 package com.espol.ed_p1_grupo07;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
